a3c34b5e10595aa76cdb5b616ee307929465ed23
Kim Kyeongho <sgn07124@gmail.com>
2025-07-16 22:28:11 +0900

    initial commit

---
+++ src/test/java/com/project/InsightPrep/InsightPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.InsightPrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
