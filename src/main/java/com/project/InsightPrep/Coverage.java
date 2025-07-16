a65e3bd288bc7df1dcea95ac786fc69172c6aa7d
Kim Kyeongho <sgn07124@gmail.com>
2025-07-17 00:46:17 +0900

    test: codecov comment test #3

---
+++ src/main/java/com/project/InsightPrep/Coverage.java
@@ -0,0 +1,16 @@
+package com.project.InsightPrep;
+
+public class Coverage {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public boolean isPositive(int number) {
+        return number > 0;
+    }
+}
