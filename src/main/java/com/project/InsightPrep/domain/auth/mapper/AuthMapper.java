53bb95c25e5fc0376624010b6889609fb031ebe3
Kim Kyeongho <sgn07124@gmail.com>
2025-07-23 01:29:54 +0900

    feat: MyBatis 파일 구성 #12

---
+++ src/main/java/com/project/InsightPrep/domain/auth/mapper/AuthMapper.java
@@ -0,0 +1,10 @@
+package com.project.InsightPrep.domain.auth.mapper;
+
+import com.project.InsightPrep.domain.member.entity.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AuthMapper {
+
+    void insertMember(Member member);
+}
