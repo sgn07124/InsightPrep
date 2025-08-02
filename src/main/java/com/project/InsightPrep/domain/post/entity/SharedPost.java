a3a25ed7a514190c4db75cab336ab1f427ef7bae
Kim Kyeongho <sgn07124@gmail.com>
2025-08-03 01:59:44 +0900

    fix: 엔티티 수정 #16

---
+++ src/main/java/com/project/InsightPrep/domain/post/entity/SharedPost.java
@@ -0,0 +1,55 @@
+package com.project.InsightPrep.domain.post.entity;
+
+import com.project.InsightPrep.domain.member.entity.Member;
+import com.project.InsightPrep.domain.question.entity.Answer;
+import com.project.InsightPrep.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shared_post")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class SharedPost extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    // 게시글 본문
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    // 공유된 답변
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    // 작성자
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    // 게시글 상태: OPEN or RESOLVED
+    @Enumerated(EnumType.STRING)
+    private PostStatus status = PostStatus.OPEN;
+}
